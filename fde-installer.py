391ebab7e0ee5e0328492ea349e2d08ab10d0c91
Xelron <kyryloorel@gmail.com>
2026-04-21 19:06:15 +0100

    FDE Installer Wizard
    this helps you install FDE without typing anything!

---
+++ fde-installer.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+import os
+
+def install():
+    tk.Label(page2, text="Preparing...").pack()
+    root.update()
+
+    os.system("sudo apt install -y git")
+
+    tk.Label(page2, text="Installing LightDM...").pack()
+    root.update()
+
+    os.system("sudo apt install -y lightdm")
+
+    tk.Label(page2, text="Cloning FDE...").pack()
+    root.update()
+
+    os.system("git clone --branch v1.0 --depth 1 https://github.com/Xelron161/FDE-desktop")
+
+    tk.Label(page2, text="Installing package...").pack()
+    root.update()
+
+    os.system("sudo dpkg -i FDE-desktop/fde.deb")
+
+    tk.Label(page2, text="Fixing dependencies...").pack()
+    root.update()
+
+    os.system("sudo apt --fix-broken install -y")
+
+    tk.Label(page2, text="Done!", font=("Segoe UI", 16)).pack()
+    tk.Label(page2, text="Reboot and select FDE at login.").pack()
+
+
+root = tk.Tk()
+root.geometry("400x300")
+root.title("FDE Installer Wizard")
+
+page1 = tk.Frame(root)
+page2 = tk.Frame(root)
+
+for p in (page1, page2):
+    p.place(relwidth=1, relheight=1)
+
+tk.Label(page1, text="Welcome to FDE Installer Wizard!", font=("Segoe UI", 20)).pack()
+tk.Label(page1, text="This Wizard is going to install FDE! 🪄", font=("Segoe UI", 10)).pack()
+
+tk.Button(page1, text="Next", command=lambda: page2.tkraise()).pack(side="bottom", anchor="e")
+
+tk.Label(page2, text="Click Install!", font=("Segoe UI", 20)).pack()
+tk.Button(page2, text="Install", command=install).pack(side="bottom", anchor="e")
+
+page1.tkraise()
+root.mainloop()
